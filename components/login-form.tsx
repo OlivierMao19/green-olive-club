fd0084edc62b0c0db454143dcd2caa5c940c57a0
Olivier Mao <124587492+OlivierMao19@users.noreply.github.com>
2025-03-05 14:32:00 -0500

    Small update

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -67,27 +67,9 @@ export function LoginForm({
                   required
                 />
               </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
               <Button type="submit" className="w-full">
                 Login
               </Button>
-            </div>
-            <div className="text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
             </div>
           </div>
         </CardContent>
