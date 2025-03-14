7fd382efc2cbc5db695b36bdd88fe746ac3ec5a5
Olivier Mao <124587492+OlivierMao19@users.noreply.github.com>
2025-03-14 10:10:24 -0400

    Small fix

--- app/(root)/events/[eventId]/page.tsx
+++ app/(root)/events/[eventId]/page.tsx
@@ -10,14 +10,14 @@ export default async function EventPage({ params }: { params: { eventId: string 
         <div className="container mx-auto px-2 py-6 md:px-4 md:w-9/10 sm:w-full mt-6">
             <Card className="h-[75svh]">
                 <CardContent className="flex flex-col items-between justify-center py-12 space-y-10">
-                    <h1 className="text-3xl font-bold">{event.title}</h1>
+                    <h1 className="text-3xl font-bold">{event!.title}</h1>
                     <p className="text-gray-600 mt-2">Description: </p>
-                    <p>{event.description}</p>
+                    <p>{event!.description}</p>
                     <p className="text-gray-500 mt-4">
-                        Location: {event.location}
+                        Location: {event!.location}
                     </p>
                     <p className="text-gray-500 mt-4">
-                        Scheduled At: {new Date(event.scheduledAt).toLocaleString()}
+                        Scheduled At: {new Date(event!.scheduledAt).toLocaleString()}
                     </p>
                 </CardContent>
             </Card>
