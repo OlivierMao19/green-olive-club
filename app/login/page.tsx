2dcb6727b1528077fab0bab2f80a6f540ad21005
Olivier Mao <124587492+OlivierMao19@users.noreply.github.com>
2025-04-07 20:45:38 -0400

    login feature

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,14 @@ import { AtSign } from "lucide-react"
 
 import { LoginForm } from "@/components/login-form"
 import Link from "next/link"
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth()
+  if (session) {
+    redirect("/")
+  }
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 bg-gradient-to-b from-green-100/80 to-white text-gray-700">
       <div className="flex w-full max-w-lg flex-col gap-6 text-green-700">
